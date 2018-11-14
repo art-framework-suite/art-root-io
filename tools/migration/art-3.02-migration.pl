9f04fa24dfb7ccbe80d0b0282f82e60d51dfc3d2
Kyle Knoepfel <knoepfel@fnal.gov>
2018-11-14 14:15:03 -0600

    Improve migration script.

--- tools/migration/art-3.02-migration.pl
+++ tools/migration/art-3.02-migration.pl
@@ -25,24 +25,32 @@ if (scalar @ARGV > 1) {
   $dry_run = $ARGV[1];
 }
 
+sub replace_in_files {
+  my ($files_ref, $old_pattern, $new_pattern, $dry) = @_;
+  foreach (@{$files_ref}) {
+    my $output = `sed -n "s|$old_pattern|$new_pattern|p" $_`;
+    if (length $output ne 0) {
+      if ($dry) {
+        print "  Would change file $_";
+      }
+      else {
+        print "  Updating file $_";
+        `sed -i.backup "s|$old_pattern|$new_pattern|g" $_`;
+      }
+    }
+  }
+}
+
 # Replace C++ header dependencies
 foreach my $old_inc (sort keys %header_list) {
   my $new_inc = $header_list{$old_inc};
   print "Checking for files using $old_inc\n";
   my $cpp_files_str = `find $top_level_dir \\( \\( -name .svn -o -name .git -o -name CVS \\) -prune \\) -o \\( -name '*.c' -o -name '*.cxx' -o -name '*.cc' -o -name '*.cpp' -o -name '*.C' -o -name '*.h' -o -name '*.hxx' -o -name '*.hh' -o -name '*.hpp' -o -name '*.[it]cc' -o -name '*.H*' \\) -print`;
   my @cpp_files = split /^/m, $cpp_files_str;
-  foreach (@cpp_files) {
-      my $output = `sed -n "s|^\\(#include\\s\\+\\"\\)$old_inc|\\1$new_inc|p" $_`;
-      if (length $output ne 0) {
-        if ($dry_run) {
-          print "  Would change file $_";
-        }
-        else {
-          print "  Updating file $_";
-          `sed -i.backup "s|^\\(#include\\s\\+\\"\\)$old_inc|\\1$new_inc|g" $_`;
-        }
-      }
-  }
+  # This pattern also removes trailing whitespace
+  my $old_pattern = '^\\(#include\\s\\+\\"\\)' . $old_inc . '[^\\s]*$';
+  my $new_pattern = '\\1' . $new_inc;
+  replace_in_files(\@cpp_files, $old_pattern, $new_pattern, $dry_run);
 }
 
 # Replace Library link lines
@@ -51,16 +59,5 @@ foreach my $old_lib (sort keys %library_list) {
   print "Checking for uses of $old_lib\n";
   my $cmake_files_str = `find $top_level_dir \\( \\( -name .svn -o -name .git -o -name CVS \\) -prune \\) -o \\( -name 'CMakeLists.txt' -o -name '*.cmake' -o -name 'SConscript' \\) -print`;
   my @cmake_files = split /^/m, $cmake_files_str;
-  foreach (@cmake_files) {
-      my $output = `sed -n "s|$old_lib|$new_lib|p" $_`;
-      if (length $output ne 0) {
-        if ($dry_run) {
-          print "  Would change file $_";
-        }
-        else {
-          print "  Updating file $_";
-          `sed -i.backup "s|$old_lib|$new_lib|g" $_`;
-        }
-      }
-  }
+  replace_in_files(\@cmake_files, $old_lib, $new_lib, $dry_run);
 }
