53d8bde7424fe76073bf80f6d1814e981188c230
Kyle Knoepfel <knoepfel@fnal.gov>
2023-11-03 16:19:21 -0500

    Use cet_transitive_paths to setup paths for tests

--- art_root_io/test/fastclonefail/v11/test_fastclonefail_r.sh
+++ art_root_io/test/fastclonefail/v11/test_fastclonefail_r.sh
@@ -8,7 +8,7 @@ export LD_LIBRARY_PATH=..${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export DYLD_LIBRARY_PATH=..${DYLD_LIBRARY_PATH:+:${DYLD_LIBRARY_PATH}}
 export CET_PLUGIN_PATH=..${CET_PLUGIN_PATH:+:${CET_PLUGIN_PATH}}
 
-art -c "${1}" -s "${2}" --rethrow-all
+art_exec=$1
+$art_exec -c "${2}" -s "${3}" --rethrow-all
 
 check_files "copy.root"
-
